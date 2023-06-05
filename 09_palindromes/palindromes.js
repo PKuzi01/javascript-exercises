c86fa21868215437d8def9b6fcc6c7d8c98b538a
P.Kuzwayo <cre8storeese@gmail.com>
2023-06-05 12:44:05 +0200

    completed functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerCase = string.toLowerCase('');
+    let removePunctuation = lowerCase.replace(/[^a-z]/g, "")
+    let splitResult = removePunctuation.split('');
+    let reverseResult = splitResult.reverse();
+    let finalResult = reverseResult.join('');
+    
+    if (finalResult == removePunctuation) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(post) {
+    if (post < 0) {
+        return `Nope, we don't accept negatives.`;
+    } else if (post == 0) {
+        return 0;
+    } else if (post == 1) {
+        return 1;
+    } else {
+        return fibonacci(post - 1) + fibonacci(post - 2);
+    }
 };
 
 // Do not edit below this line
